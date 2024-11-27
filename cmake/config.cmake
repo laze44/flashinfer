631fdf0abf57b95245e182f14040e9cffaa83278
laze44 <lizh0404@outlook.com>
2024-11-27 18:17:40 +0800

    finish constant batch prefill func

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -21,7 +21,7 @@ set(FLASHINFER_FASTDIV_TEST ON)
 # Whether to compile fastdequant tests
 set(FLASHINFER_FASTDEQUANT_TEST ON)
 # Whether to compile distributed tests
-set(FLASHINFER_DISTRIBUTED ON)
+set(FLASHINFER_DISTRIBUTED OFF)
 # The following configurations can impact the binary size of the generated
 # library
 set(FLASHINFER_GEN_HEAD_DIMS 64 128 256 512)
